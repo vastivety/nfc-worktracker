bd940304f665733c93512a6e023ba43309c21597
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-03-10 17:49:49 -0500

    made notification for service. stop button does not work yet

--- app/src/main/java/ca/mcgill/nfcworktracker/NfcService.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/NfcService.kt
@@ -3,8 +3,12 @@ package ca.mcgill.nfcworktracker
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
@@ -12,13 +16,15 @@ import android.nfc.Tag
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.content.res.ResourcesCompat
 import java.util.*
 
 class NfcService : Service() {
 
     companion object {
-        val SERVICE_NOTIFICATION_CHANNEL_ID: String = "service-notification-channel"
-        val SERVICE_NOTIFICATION_CHANNEL_NAME: String = "Tracking Active"
+        const val SERVICE_NOTIFICATION_CHANNEL_ID = "service-notification-channel"
+        const val SERVICE_NOTIFICATION_CHANNEL_NAME = "Tracking Active"
+        const val ACTION_STOP_SELF = "broadcast intent to stop self"
     }
 
     override fun onBind(intent: Intent): IBinder? {
@@ -48,13 +54,34 @@ class NfcService : Service() {
         //start service
         (getSystemService(NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(NotificationChannel(
             SERVICE_NOTIFICATION_CHANNEL_ID, SERVICE_NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT))
+
+        val stopIntent = Intent(this, NotificationActionBroadcastReceiver::class.java).apply {
+            action = ACTION_STOP_SELF
+        }
+        val stopAction = Notification.Action.Builder(
+            Icon.createWithResource(this, R.drawable.ic_stop_circle),
+            "CANCEL",
+            PendingIntent.getBroadcast(this, 0, stopIntent, PendingIntent.FLAG_IMMUTABLE)
+        ).build()
         val nf: Notification = Notification.Builder(this, SERVICE_NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_nfc)
             .setContentTitle("test")
             .setContentText("testtttttttext")
+            .addAction(stopAction)
             .build()
         startForeground(1, nf)
 
-        return super.onStartCommand(intent, flags, startId)
+        return START_NOT_STICKY
+    }
+
+    private inner class NotificationActionBroadcastReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("nfc", "received with ${intent?.action}")
+            if (intent?.action.equals(ACTION_STOP_SELF)) {
+                this@NfcService.stopSelf()
+            }
+        }
+
     }
 
     private fun getRelevantRecord(messages: Array<NdefMessage>): NdefRecord? {

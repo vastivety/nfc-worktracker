f3125e8fd00ae386eecfa43fbe3202c42d599472
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-03-26 14:06:05 -0400

    added/configured main activity

--- app/src/main/java/ca/mcgill/nfcworktracker/MainActivity.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import android.Manifest
+import android.widget.Toast
+import ca.mcgill.nfcworktracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
@@ -17,6 +21,10 @@ class MainActivity : AppCompatActivity() {
             ) {
                 requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1234)
             }
+        }
+
+        binding.showPrevious.setOnClickListener {
+            Toast.makeText(this, "moin", Toast.LENGTH_SHORT).show()
         }
     }
 }

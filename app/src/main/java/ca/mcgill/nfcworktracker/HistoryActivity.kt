7f9c9ee8a6656ccdcdacda128be1e0d293fa6732
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-03-30 13:01:24 -0400

    moved history clear to menu bar (rather than having a button)

--- app/src/main/java/ca/mcgill/nfcworktracker/HistoryActivity.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/HistoryActivity.kt
@@ -2,6 +2,9 @@ package ca.mcgill.nfcworktracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import ca.mcgill.nfcworktracker.databinding.ActivityHistoryBinding
 import ca.mcgill.nfcworktracker.history.HistoryAdapter
 import ca.mcgill.nfcworktracker.history.HistoryDataPoint
@@ -9,18 +12,35 @@ import ca.mcgill.nfcworktracker.history.HistoryDatabaseHelper
 import java.time.Instant
 
 class HistoryActivity : AppCompatActivity() {
+    private lateinit var adapter: HistoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = HistoryAdapter(HistoryDatabaseHelper(application as MyApplication))
+        adapter = HistoryAdapter(HistoryDatabaseHelper(application as MyApplication))
         binding.recyclerView.adapter = adapter
 
-        binding.clearTestButton.setOnClickListener { adapter.clearHistory() }
         binding.addTestButton.setOnClickListener { adapter.addPoint(
             HistoryDataPoint(Instant.now().minusSeconds(400), Instant.now())
         ) }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.history_appbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.clear_history -> {
+                adapter.clearHistory()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }

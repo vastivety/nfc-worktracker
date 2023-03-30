b35c7f5caa0866b027190059e7dd70095cc1d79b
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-03-30 15:10:12 -0400

    added fragment navigation for creation flow

--- app/src/main/java/ca/mcgill/nfcworktracker/CreateTagActivity.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/CreateTagActivity.kt
@@ -3,9 +3,7 @@ package ca.mcgill.nfcworktracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import ca.mcgill.nfcworktracker.create.IntroFragment
+import androidx.navigation.findNavController
 import ca.mcgill.nfcworktracker.databinding.ActivityCreateTagBinding
 
 class CreateTagActivity : AppCompatActivity() {
@@ -15,19 +13,15 @@ class CreateTagActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<IntroFragment>(R.id.fragment_container_view)
-            }
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             android.R.id.home -> {
-                this.finish()
+                // Navigate back. If no previous fragment is found, return to previous activity.
+                if(!findNavController(R.id.create_tag_fragment_container).popBackStack()) {
+                    this.finish()
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/ca/mcgill/nfcworktracker/create/IntroFragment.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/create/IntroFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ca.mcgill.nfcworktracker.R
+import ca.mcgill.nfcworktracker.databinding.FragmentCreateIntroBinding
 
 class IntroFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_create_intro, container, false)
+    ): View {
+        val binding = FragmentCreateIntroBinding.inflate(layoutInflater)
+
+        binding.next.setOnClickListener {
+            findNavController().navigate(R.id.action_introFragment_to_setupFragment)
+        }
+
+        return binding.root
     }
 }

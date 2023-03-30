b6916cc113979227d47b0c7435cdb1c2591a5200
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-03-30 16:55:45 -0400

    moved home(back) button action to also cover system back button (USES DEPRECATED)

--- app/src/main/java/ca/mcgill/nfcworktracker/CreateTagActivity.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/CreateTagActivity.kt
@@ -18,22 +18,26 @@ class CreateTagActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             android.R.id.home -> {
-                val navController = findNavController(R.id.create_tag_fragment_container)
-                when (navController.currentDestination?.id) {
-                    // Prevent going back to write fragment
-                    R.id.confirmationFragment -> this.finish()
-                    // Follow retry path.
-                    R.id.failureFragment -> navController.navigate(R.id.action_failureFragment_retry)
-                    // Default behaviour: navigate up. If no previous fragment is found, return to previous activity.
-                    else -> {
-                        if(!navController.popBackStack()) {
-                            this.finish()
-                        }
-                    }
-                }
+                onBackPressed()
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        val navController = findNavController(R.id.create_tag_fragment_container)
+        when (navController.currentDestination?.id) {
+            // Prevent going back to write fragment
+            R.id.confirmationFragment -> this.finish()
+            // Follow retry path.
+            R.id.failureFragment -> navController.navigate(R.id.action_failureFragment_retry)
+            // Default behaviour: navigate up. If no previous fragment is found, return to previous activity.
+            else -> {
+                if(!navController.popBackStack()) {
+                    this.finish()
+                }
+            }
         }
     }
 }

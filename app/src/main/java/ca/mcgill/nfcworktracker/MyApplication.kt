6fa9b47ecf484f8823a7582200b8768b0c716afc
Torge Rosendahl <torge.rosendahl@mail.mcgill.ca>
2023-04-13 00:14:38 -0400

    added material 3 dynamic colors

--- app/src/main/java/ca/mcgill/nfcworktracker/MyApplication.kt
+++ app/src/main/java/ca/mcgill/nfcworktracker/MyApplication.kt
@@ -2,10 +2,17 @@ package ca.mcgill.nfcworktracker
 
 import android.app.Application
 import ca.mcgill.nfcworktracker.database.AppDatabase
+import com.google.android.material.color.DynamicColors
 
 class MyApplication : Application() {
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
     private val database by lazy { AppDatabase.getDatabase(this) }
     val history by lazy { database.historyEntryDao() }
+
+    override fun onCreate() {
+        super.onCreate()
+        // Apply material 3 dynamic colors
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
